13a6f291365c5f578a62d05dc14512bf889f1773
Egor-Zhmaylik <93212089+Egor-Zhmaylik@users.noreply.github.com>
2025-06-17 13:04:54 +0300

    Update check_auth.php

--- check_auth.php
+++ check_auth.php
@@ -2,12 +2,11 @@
 session_start();
 $response = array();
 
-// Проверяем, есть ли пользователь в сессии
 if (isset($_SESSION['user'])) {
     $response['isAuthenticated'] = true;
 } else {
     $response['isAuthenticated'] = false;
 }
 
-echo json_encode($response); // Возвращаем статус в формате JSON
+echo json_encode($response); 
 ?>
